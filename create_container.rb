d2b5b31fd440376cc304dd1cf0fe897adc452c74
hs366 <sabzian@gmail.com>
2015-02-26 09:56:06 +0100

    general method _request method added

---
+++ create_container.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/ruby
+require 'json'
+require 'socket'
+require 'excon'
+require 'awesome_print'
+require 'logger'
+
+$logger = Logger.new($stdout)
+$logger.level = Logger::DEBUG 
+
+def _request(htmethod, endpoint, options = {}, body = "")
+  rs = endpoint
+  unless options == {}
+    options.each {|r| rs += "&" + 
+      URI.escape(r[0].to_s) + "=" + 
+      URI.escape(r[1].to_s) }
+  end
+  #rs = URI.parse(rs)
+  $logger.debug("docker/_request htmethod=#{htmethod} endpint=#{endpoint}, request=unix:///#{rs}, body=#{body}")
+
+  case htmethod
+  when :get
+    response = Excon.get("unix:///#{rs}", :socket => '/var/run/docker.sock')
+  when :post
+    response = Excon.post("unix:///#{rs}", 
+                          :socket => '/var/run/docker.sock', 
+                          :body => body, 
+                          :headers => {"Content-Type" => "application/json"})
+  when :delete
+    response = Excon.delete("unix:///#{rs}", 
+                            :socket => '/var/run/docker.sock', 
+                            :body => body, 
+                            :headers => {"Content-Type" => "application/json"})
+  else
+    raise "Internal error"
+  end
+  # $logger.debug "this is response body#{response.body}"
+  case response.status
+  when 200
+    return response.status
+  when 201
+    return response.body
+  when 204
+    return response.body
+  else
+    raise "Unable to fulfill request. HTTP status #{response.status}"
+  end
+end
+
+config = '{"Cmd": "date", "Image": "ubuntu"}'
+
+get_response = _request(:post, 'containers/create', {}, config )
+#get_response = _request(:get, 'version')
+
+ap JSON.parse(get_response)
+
