69d5b3a405ee07a4f4987829412b9ef9bbb55735
Hamed <hamsa339@student.liu.se>
2015-03-04 12:10:23 +0100

    Update container.rb

--- container.rb
+++ container.rb
@@ -6,71 +6,21 @@ require 'awesome_print'
 
 include MCollective::RPC
 
-
 mc = rpcclient("docker")
 mc.progress = true
 mc.discover :nodes => ["host1"]
 
-=begin
+config ='{"Image": "ubuntu", "Cmd": "date"}'
 
-config = {
-  :Hostname => "492510dd38e4",
-  :Domainname => "",
-  :User => "",
-  :Memory => 0,
-  :MemorySwap => 0,
-  :CpuShares => 512,
-  :Cpuset => "0,1",
-  :AttachStdin => false,
-  :AttachStdout => true,
-  :AttachStderr => true,
-  :PortSpecs => nil,
-  :Tty => false,
-  :OpenStdin => false,
-  :StdinOnce => false,
-  :Env => nil,
-  :Cmd => ["echo 123"],
-  :Image => "c4ff7513909dedf4ddf3a450aea68cd817c42e698ebccf54755973576525c416",
-  :Volumes => {"/tmp" => {} },
-  :WorkingDir => "",
-  :NetworkDisabled => false,
-  :ExposedPorts => {"22/tcp" => {} },
-  :RestartPolicy => { "elegant_hypatia" => "always" }
-}
-=end
-
-config ='{
-  "Hostname":"492510dd38e4",
-  "Image":"c4ff7513909dedf4ddf3a450aea68cd817c42e698ebccf54755973576525c416",
-  "Cmd":"date"
-}'
-=begin
-
-puts JSON.parse([ 'foo' ].to_json).first
-
-=begin
-config = '{
-  \"Hostname\": \"492510dd38e\",
-  \"Image\": \"c4ff7513909dedf4ddf3a450aea68cd817c42e698ebccf54755973576525c416\",
-  \"Cmd\": \"date\",
-}'
-=end
-
+#puts JSON.parse([ 'foo' ].to_json).first
 #rpc_res = mc.createcontainer(:name => "ubuntu" , :config => JSON.generate([config]))
-rpc_res = mc.createcontainer(:name => "ubuntu" , :config => config.to_json)
 
+rpc_res = mc.createcontainer(:name => "test1234" , :config => config)
 rpc_res.each do |rpc_obj|
  result = rpc_obj.results || {}
-
-
-#    ap JSON.parse(result[:data][:details])
-#    puts result[:data][:id]
-
  puts result.inspect
  puts result[:data][:id]
 end
-
-
 
 
 printrpcstats
